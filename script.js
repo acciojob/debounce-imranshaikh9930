874457314acbffa96c7ddb1d2c651b2724999b09
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-23 14:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function debounce(callback, delay, immediate = false) {
+			let timeoutId;
+		return function (..args){
+
+			const context = this;
+			
+			const later = ()=>{
+				timeoutId = null;
+
+				if(!immediate){
+					
+				callback.apply(context,args);
+				}
+			}
+			const callNow = immediate && !timeoutId;
+
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(later, delay);
+
+        if (callNow) {
+            callback.apply(context, args);
+        }
+		};
   
   }
   
